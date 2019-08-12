bfe1163f7da24840582c73dbb2d81759efc04116
Marknjo <marknjo7@gmail.com>
2019-08-12 12:15:53 +0300

    Updates

---
+++ precache-manifest.35c296af91bc5788fa9f2543f9fb1f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8ca3349cca438b230676f2d9b126a0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d44be37644e588f51849",
+    "url": "/monsters-rolodex/static/css/main.fb10326d.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monsters-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "d44be37644e588f51849",
+    "url": "/monsters-rolodex/static/js/main.e465e099.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
